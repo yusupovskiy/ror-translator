34d82b7ba1d94b27715cfd06aec4b7a010577699
yusupovskiy <yusupovskiy.rasul@mail.ru>
2019-06-02 11:34:19 +0300

    Customize homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
   get 'langs' => 'application#get_langs'
   get 'translate' => 'application#get_translate'
   get 'detect' => 'application#get_detect'
